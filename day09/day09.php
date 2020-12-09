85b1ed8085f293bc0b3ae012adf93b262cb52f95
Jérôme <jvillafruela@users.noreply.github.com>
2020-12-10 00:20:50 +0100

    day 09 part 2

--- day09/day09.php
+++ day09/day09.php
@@ -3,8 +3,6 @@
 
 //tests(); exit();
 
-
-
 $numbers=array();
 
 $file=file(dirname(__FILE__). '/input9.txt',FILE_IGNORE_NEW_LINES);
@@ -13,6 +11,7 @@ foreach ($file as $i => $line) {
 }
 
 $answer1=answer1($numbers,25); 
+$answer2=answer2($numbers,$answer1); 
 
 function answer1($numbers,$len) {
     for($i=$len;$i<count($numbers);$i++) {
@@ -30,10 +29,37 @@ function answer1($numbers,$len) {
             }
         }
         if ($ok==false) {
-            print "DDD anser $target \n";
+            print "DDD answer $target \n";
             return $target;
         }
     }
     return false;
 }
 
+function answer2($numbers, $target) {
+    $len=count($numbers);
+    $ok=false;
+    for ($i=0;$i<$len;$i++) {
+        $sum=0;
+        for ($j=$i;$j<$len;$j++) {
+            $sum += $numbers[$j];
+            //print("DDD $target sum[$i..$j] = $sum\n");
+            if ($sum==$target) {
+                $ok=true;
+                break 2;
+            }
+        }
+    }
+    if($ok) {
+        print("DDD found $target = sum[$i..$j] \n");
+        $min=$numbers[$i];
+        $max=$numbers[$i];
+        for($k=$i;$k<=$j;$k++) {
+            if($numbers[$k]<$min) $min=$numbers[$k];
+            if($numbers[$k]>$max) $max=$numbers[$k];            
+        }
+        $answer2 = $min + $max;
+        print("DDD min=$min max=$max answer2=$answer2\n");       
+    } 
+    return false;
+}
